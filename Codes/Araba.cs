8a91546b3eaa93a877778c9bedf07f7b5eb8768c
Denyazor <denizortas17@gmail.com>
2024-11-12 23:25:40 +0300

    Add files via upload

---
+++ Codes/Araba.cs
@@ -0,0 +1,10 @@
+public class Program
+{
+    public static void Main()
+    {
+        Araba araba1 = new Araba("Nissan Skyline R34 ", 1999, "Sky Blue");
+
+        araba1.BilgiYazdır();
+        araba1.BilgiYazdır();
+    }
+}
